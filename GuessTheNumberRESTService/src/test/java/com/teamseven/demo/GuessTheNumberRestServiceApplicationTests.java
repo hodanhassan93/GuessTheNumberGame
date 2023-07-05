7d1965c1906544c2146f013816c688e3379bce8b
Maria Ouvarova <meo2112@hotmail.com>
2023-07-05 09:19:27 +0100

    Test updated

--- GuessTheNumberRESTService/src/test/java/com/teamseven/demo/GuessTheNumberRestServiceApplicationTests.java
+++ GuessTheNumberRESTService/src/test/java/com/teamseven/demo/GuessTheNumberRestServiceApplicationTests.java
@@ -1,13 +1,33 @@
 package com.teamseven.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.teamseven.dto.entity.Game;
+import com.teamseven.model.persistence.GameDao;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GuessTheNumberRestServiceApplicationTests {
+	
+	@Autowired
+	private GameDao gameDao;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testUpdateGame(int gameId,boolean status){
+		assertEquals(0, gameDao.updateGame(2, false));
+	}
+	
 }
+
+
